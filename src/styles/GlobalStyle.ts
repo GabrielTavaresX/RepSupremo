e2c15279d2f8660ac67aa32ef7275039bb7c62d6
GabrielTavaresX <gabrielpiche13@gmail.com>
2023-11-28 14:11:17 -0300

    comentário

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+import 'react-toastify/dist/ReactToastify.css';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+  
+`
+export const colors = {
+  primary: "#2B75E2",
+  secondary: "#FFC107",
+  secondaryLight: "#f0dea5",
+  third: "#78909C",
+  thirdLight: "#d9d9d9",
+  black: "#000",
+  white: "#fff"
+}
